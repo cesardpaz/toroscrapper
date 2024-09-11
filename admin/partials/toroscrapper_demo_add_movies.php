057b4b0481a29eb3b66e0ccc91b618b564d7fe54
cesardpaz <cesardpaz@gmail.com>
2024-09-11 14:09:28 -0500

    Design Add movies

--- admin/partials/toroscrapper_demo_add_movies.php
+++ admin/partials/toroscrapper_demo_add_movies.php
@@ -1,32 +1,5 @@
-
-<div>
-
-<h5>Add Movies</h5>
-<hr>
-
-
-<div class="row">
-    <div class="col s12">
-        <div class="row">
-            <div class="input-field col s12">
-                <i class="material-icons prefix">download</i>
-                <textarea id="movies_id_tmdb" class="materialize-textarea" cols="30" rows="6"></textarea>
-                <label for="movies_id_tmdb">IDs of TMDB</label>
-            </div>
-        </div>
-    </div>
-
-   
-
-    <div class="col s12">
-        <div class="row">
-            <a id="add-movies" class="waves-effect waves-light btn">Add Movies</a>
-        </div>
-    </div>
-
-    
+<h5 class="text-xl font-bold uppercase">Add movies</h5>
+<p class="text-sm text-gray-500">Click in button for generate 100 movies</p>
+<div class="mt-4">
+    <a id="add-movies" class="bg-blue-600 hover:text-white text-white px-6 py-2 rounded uppercase">Add Movies</a>
 </div>
-
-
-</div>
-
--- changelog.md
+++ changelog.md
@@ -1,16 +1,11 @@
-
 ## [1.0.0] - 2022-06-20
-
 ### Added
-
 - Tailwind framework
 - NPM  
 - File .gitignore
-
 ### Fixed
-
 - Fixed item
-
 ### Removed
-
 - Materialize framework
+
+## [1.0.1] - 2024-09-11
